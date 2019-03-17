85a7b0d122b78c71180b909853345d126c3caf3e
Ivan Barrio <ibarriom@uoc.edu>
2019-03-17 18:05:23 +0100

    Camera & Battleground
    Mapa de batalla prototipat amb un personatge no interactuable i diferents objectes d'escenari provisional.
    Moviment de la camara en els eixos X i Z establert amb les tecles WASD i la creueta

---
+++ Assets/Tales_from_Nahelm/Scripts/CameraController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    //Falta afegir el moviment de rotació de la camara per a les tecles "q" i "e"
+
+    float mainSpeed = 100.0f;   //Velocitat de moviment de la camara
+    float shiftAdd = 250.0f;    //Multiplicat per el temps que s'ha apretat el shift
+    float maxShift = 1000.0f;
+    float camSens = 0.25f;
+    private Vector3 lastMouse = new Vector3(255, 255, 255);
+    private float totalRun = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float f = 0.0f;
+        Vector3 p = GetBaseInput();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            totalRun += Time.deltaTime;
+            p = p * totalRun * shiftAdd;
+            p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+            p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+            p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+        }
+        else
+        {
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+            p = p * mainSpeed;
+        }
+
+        p = p * Time.deltaTime;
+        Vector3 newPosition = transform.position;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(p);
+            newPosition.x = transform.position.x;
+            newPosition.z = transform.position.z;
+            transform.position = newPosition;
+        }
+        else
+        {
+            transform.Translate(p);
+        }
+
+    }
+
+    private Vector3 GetBaseInput()
+    {
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+
+        return p_Velocity;
+    }
+}
