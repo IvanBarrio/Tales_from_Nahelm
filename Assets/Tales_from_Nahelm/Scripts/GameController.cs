5b08c08585e9ad84fb235cc673283f6624f88939
Ivan Barrio <ibarriom@uoc.edu>
2019-04-02 19:30:55 +0200

    Repas Estadistiques i neteja de codi
    Introduides les estadistiques inicials reals dels personatges, de l'enemic final del mapa i d'alguna de les armes disponibles.
    Realitzada neteja de codi innecessari de les proves inicials de moviment i d'atac.

--- Assets/Tales_from_Nahelm/Scripts/GameController.cs
+++ Assets/Tales_from_Nahelm/Scripts/GameController.cs
@@ -29,23 +29,23 @@ public class GameController : MonoBehaviour
             switch (unit.name)
             {
                 case "Niva":
-                    unit.GetComponent<Character>().createCharacter("Ally", "Niva", "Fighter", 20, 8, 0, 5, 5, 0, 4, 0, 20, 1);//todo
-                    unit.GetComponent<Weapon>().setWeapon("Armads", "Axe", "A", 17, 1, 80, 10);
+                    unit.GetComponent<Character>().createCharacter("Ally", "Niva", "Captain", 23, 13, 5, 9, 11, 8, 8, 5, 20, 1);//faltan los crecimientos y los totales
+                    unit.GetComponent<Weapon>().setWeapon("Northern Axe", "Axe", "A", 10, 1, 65, 0);
                     unit.GetComponent<Character>().setWeapon(unit.GetComponent<Weapon>());
                     break;
                 case "Aki":
-                    unit.GetComponent<Character>().createCharacter("Ally", "Aki", "Assassin", 21, 8, 0, 13, 12, 0, 5, 1, 20, 1);//todo
-                    unit.GetComponent<Weapon>().setWeapon("Armads", "Axe", "A", 17, 1, 80, 10);//todo
+                    unit.GetComponent<Character>().createCharacter("Ally", "Aki", "Nomad", 23, 8, 6, 10, 12, 7, 7, 7, 20, 1);//faltan los crecimientos y los totales
+                    unit.GetComponent<Weapon>().setWeapon("Light Blade", "Sword", "A", 9, 1, 90, 30);
                     unit.GetComponent<Character>().setWeapon(unit.GetComponent<Weapon>());
                     break;
                 case "Hilda":
-                    unit.GetComponent<Character>().createCharacter("Ally", "Hilda", "Berserk", 22, 8, 4, 7, 9, 0, 7, 10, 30, 1);//todo
-                    unit.GetComponent<Weapon>().setWeapon("Armads", "Axe", "A", 17, 1, 80, 10);//todo
+                    unit.GetComponent<Character>().createCharacter("Ally", "Hilda", "Berserk", 31, 14, 4, 6, 13, 6, 7, 4, 30, 1);//faltan los crecimientos y los totales
+                    unit.GetComponent<Weapon>().setWeapon("Siegfried's Lance", "Lance", "A", 11, 1, 80, 10);
                     unit.GetComponent<Character>().setWeapon(unit.GetComponent<Weapon>());
                     break;
                 case "Ann":
-                    unit.GetComponent<Character>().createCharacter("Ally", "Ann", "Tactician", 16, 4, 3, 5, 5, 0, 5, 3, 20, 1);//todo
-                    unit.GetComponent<Weapon>().setWeapon("Armads", "Axe", "A", 17, 1, 80, 10);//todo
+                    unit.GetComponent<Character>().createCharacter("Ally", "Ann", "Priestess", 17, 8, 10, 8, 9, 8, 8, 5, 20, 1);//faltan los crecimientos y los totales
+                    unit.GetComponent<Weapon>().setWeapon("Aestus", "Sword", "A", 10, 1, 80, 0);
                     unit.GetComponent<Character>().setWeapon(unit.GetComponent<Weapon>());
                     break;
             }
@@ -59,7 +59,7 @@ public class GameController : MonoBehaviour
             switch (unit.name)
             {
                 case "Omak":
-                    unit.GetComponent<Character>().createCharacter("Enemy", "Omak", "Warrior", 20, 8, 0, 5, 5, 0, 4, 0, 20, 1);//todo
+                    unit.GetComponent<Character>().createCharacter("Enemy", "Omak", "Warrior", 41, 18, 0, 13, 12, 11, 13, 1, 20, 1);
                     unit.GetComponent<Weapon>().setWeapon("Silver Lance", "Lance", "B", 13, 1, 75, 0);
                     unit.GetComponent<Character>().setWeapon(unit.GetComponent<Weapon>());
                     break;
@@ -108,87 +108,88 @@ public class GameController : MonoBehaviour
         /*
          * Controlar las condiciones de juego
          */
-
-        switch (turnState)
+        switch (actualTurn)
         {
-            //Estat inicial/Cerca de personatge per moure/Observacio per el mapa
-            case 'I':
-                //Si es fa click sobre un objecte
-                if (Input.GetMouseButtonDown(0))
+            case 'P':   //Torn del jugador
+                switch (turnState)
                 {
-                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                    RaycastHit hit;
-                    if (Physics.Raycast(ray, out hit))
-                    {
-                        //Si pulsem sobre una unitat del jugador en el torn del jugador passem a comprobar si es el torn de la IA i al reves
-                        if (hit.collider.tag == "Ally" && actualTurn == 'P')
+                    //Estat inicial/Cerca de personatge per moure/Observacio per el mapa
+                    case 'I':
+                        //Si es fa click sobre un objecte
+                        if (Input.GetMouseButtonDown(0))
                         {
-                            selectedCharacter = hit.collider.name;
-                            initialmovementPoint = GameObject.Find(selectedCharacter).transform.position;
-                            turnState = 'C';
-                            GameObject.Find("MovementArea").transform.position = new Vector3(initialmovementPoint.x, GameObject.Find("MovementArea").transform.position.y, initialmovementPoint.z);
-                            //disableUnit();
+                            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                            RaycastHit hit;
+                            if (Physics.Raycast(ray, out hit))
+                            {
+                                //Si pulsem sobre una unitat del jugador en el torn del jugador passem a comprobar si es el torn de la IA i al reves
+                                if (hit.collider.tag == "Ally")
+                                {
+                                    selectedCharacter = hit.collider.name;
+                                    initialmovementPoint = GameObject.Find(selectedCharacter).transform.position;
+                                    turnState = 'C';
+                                    GameObject.Find("MovementArea").transform.position = new Vector3(initialmovementPoint.x, GameObject.Find("MovementArea").transform.position.y, initialmovementPoint.z);
+                                }
+                            }
                         }
-                        if (hit.collider.tag == "Enemy" && actualTurn == 'A')
+                        break;
+                    //Personatge seleccionat i disponible per a moure
+                    case 'C':
+                        if (Input.GetMouseButtonDown(0))
                         {
+                            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                            RaycastHit hit;
+
+                            if (Physics.Raycast(ray, out hit))
+                            {
+                                destination = new Vector3(hit.point.x, 0, hit.point.z);
+                                if (Vector3.Distance(destination, initialmovementPoint) < 20)    //20 es el radi de la esfera i per tant el valor que haurem d'assignar al moviment del personatge
+                                {
+                                    GameObject.Find(selectedCharacter).GetComponent<NavMeshAgent>().SetDestination(destination);
+                                    GameObject.Find(selectedCharacter).GetComponent<NavMeshAgent>().isStopped = false;
+                                    turnState = 'A';
+                                }
+                            }
+                        }
+                        break;
+                    //Personatge mogut i amb possibilitat d'atacar
+                    case 'A':
+                        if (GameObject.Find(selectedCharacter).GetComponent<NavMeshAgent>().remainingDistance < 0.1f){
+                            GameObject.Find(selectedCharacter).GetComponent<NavMeshAgent>().isStopped = true;
+                            GameObject.Find("AtackArea").transform.position = new Vector3(GameObject.Find(selectedCharacter).transform.position.x, GameObject.Find("AtackArea").transform.position.y, GameObject.Find(selectedCharacter).transform.position.z);
+                            GameObject[] en = getEnemiesInRange(GameObject.Find(selectedCharacter).transform.position, 4f, GameObject.Find(selectedCharacter).tag); //El rang que es passa sera el rang que tingui l'arma d'atac
+                            if (en[0] != null)
+                            {
+                                Debug.Log("Es pot atacar a un enemic!");
+                                //De moment com només tenim una unitat aliada i una unitat enemiga farem que realitzi l'atac directament.
+                                Debug.Log(selectedCharacter + " es disposa a atacar a " + en[0].name);
+                                int dead = combatTurn(GameObject.Find(selectedCharacter).GetComponent<Character>(), en[0].GetComponent<Character>());
+                                switch (dead)
+                                {
+                                    case 1:
+                                        Debug.Log(en[0].name + " a matat a " + selectedCharacter + " en combat.");
+                                        break;
+                                    case 2:
+                                        Debug.Log(selectedCharacter + " a matat a " + en[0].name + " en combat.");
+                                        break;
+                                }
+                            }
+                            else
+                            {
+                                Debug.Log("No hi ha enemics a prop.");
+                            }
+                            turnState = 'I';
+                            GameObject.Find("MovementArea").transform.position = new Vector3(371, GameObject.Find("MovementArea").transform.position.y, 88);
+                            selectedCharacter = "";
                             disableUnit();
                         }
-                    }
+                        break;
                 }
                 break;
-            //Personatge seleccionat i disponible per a moure
-            case 'C':
-                if (Input.GetMouseButtonDown(0))
-                {
-                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-                    RaycastHit hit;
-
-                    if (Physics.Raycast(ray, out hit))
-                    {
-                        destination = new Vector3(hit.point.x, 0, hit.point.z);
-                        if (Vector3.Distance(destination, initialmovementPoint) < 20)    //20 es el radi de la esfera i per tant el valor que haurem d'assignar al moviment del personatge
-                        {
-                            GameObject.Find(selectedCharacter).GetComponent<NavMeshAgent>().SetDestination(destination);
-                            GameObject.Find(selectedCharacter).GetComponent<NavMeshAgent>().isStopped = false;
-                            turnState = 'A';
-                        }
-                    }
-                }
-                break;
-            //Personatge mogut i amb possibilitat d'atacar
             case 'A':
-                if (GameObject.Find(selectedCharacter).GetComponent<NavMeshAgent>().remainingDistance < 0.1f){
-                    GameObject.Find(selectedCharacter).GetComponent<NavMeshAgent>().isStopped = true;
-                    GameObject.Find("AtackArea").transform.position = new Vector3(GameObject.Find(selectedCharacter).transform.position.x, GameObject.Find("AtackArea").transform.position.y, GameObject.Find(selectedCharacter).transform.position.z);
-                    GameObject[] en = getEnemiesInRange(GameObject.Find(selectedCharacter).transform.position, 4f, GameObject.Find(selectedCharacter).tag); //El rang que es passa sera el rang que tingui l'arma d'atac
-                    if (en[0] != null)
-                    {
-                        Debug.Log("Es pot atacar a un enemic!");
-                        //De moment com només tenim una unitat aliada i una unitat enemiga farem que realitzi l'atac directament.
-                        Debug.Log(selectedCharacter + " es disposa a atacar a " + en[0].name);
-                        int dead = combatTurn(GameObject.Find(selectedCharacter).GetComponent<Character>(), en[0].GetComponent<Character>());
-                        switch (dead)
-                        {
-                            case 1:
-                                Debug.Log(en[0].name + " a matat a " + selectedCharacter + " en combat.");
-                                break;
-                            case 2:
-                                Debug.Log(selectedCharacter + " a matat a " + en[0].name + " en combat.");
-                                break;
-                        }
-                        turnState = 'I';
-                        disableUnit();
-                    }
-                    else
-                    {
-                        Debug.Log("No hi ha enemics a prop.");
-                    }
-                    turnState = 'I';
-                    GameObject.Find("MovementArea").transform.position = new Vector3(371, GameObject.Find("MovementArea").transform.position.y, 88);
-                    //GameObject.Find("AtackArea").transform.position = new Vector3(371, GameObject.Find("AtackArea").transform.position.y, 88);
-                }
-                
+                disableUnit();
+                //Todo
                 break;
         }
     }
@@ -198,26 +199,25 @@ public class GameController : MonoBehaviour
      */
     void disableUnit()
     {
-        if (Input.GetMouseButton(0))
+        unitsToMove--;
+        if (unitsToMove == 0)
         {
-            unitsToMove--;
-            if (unitsToMove == 0)
+            switch (actualTurn)
             {
-                switch (actualTurn)
-                {
-                    case 'P':
-                        actualTurn = 'A';
-                        Debug.Log("Turno " + turn + " de la IA");
-                        unitsToMove = GameObject.FindGameObjectsWithTag("Enemy").Length;
-                        break;
-                    case 'A':
-                        actualTurn = 'P';
-                        Debug.Log("Turno " + turn + " del jugador");
-                        unitsToMove = GameObject.FindGameObjectsWithTag("Ally").Length;
-                        break;
-                }
+                case 'P':
+                    actualTurn = 'A';
+                    Debug.Log("Turno " + turn + " de la IA");
+                    unitsToMove = GameObject.FindGameObjectsWithTag("Enemy").Length;
+                    break;
+                case 'A':
+                    turn++; //Cambiem de torn cada cop que acaba la IA el seu
+                    actualTurn = 'P';
+                    Debug.Log("Turno " + turn + " del jugador");
+                    unitsToMove = GameObject.FindGameObjectsWithTag("Ally").Length;
+                    break;
             }
         }
+        
     }
 
     public char getTurnState()
